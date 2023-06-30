32d8a5f422645b3c6b19d13e46061f2d4657be67
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-30 13:26:31 +0300

    changed routes

--- routes/movies.js
+++ routes/movies.js
@@ -11,7 +11,7 @@ const { getMovies, createMovie, deleteMovie } = require('../controllers/movies')
 // router.use(auth);
 
 // роуты, требующие авторизации
-router.get(getMovies);
+router.get('/movies', getMovies);
 
 router.post('/movies', validateCreateMovie, createMovie);
 
