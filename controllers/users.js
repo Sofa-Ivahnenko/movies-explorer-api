f6abc6306c93381863070e0832caa3c77bed6815
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-07-31 13:01:02 +0300

    Revert "user"
    This reverts commit 5fee4b5cd28403df1f24b8e77a83ae20d726cf7d.

--- controllers/users.js
+++ controllers/users.js
@@ -116,21 +116,22 @@
 //     }
 //   });
 
-const { NODE_ENV, JWT_SECRET = "some-secret-key" } = process.env;
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
 
-const User = require("../models/user");
-const BadRequest = require("../errors/bad-request-error");
-const NotFound = require("../errors/not-found-error");
-const ConflictError = require("../errors/conflict-error");
+const { NODE_ENV, JWT_SECRET = 'some-secret-key' } = process.env;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+const BadRequest = require('../errors/bad-request-error');
+const NotFound = require('../errors/not-found-error');
+const ConflictError = require('../errors/conflict-error');
 
 const getUser = (req, res, next) => {
   const userId = req.user._id;
 
   User.findById(userId)
     .orFail(() => {
-      throw new NotFound("Пользователь по указанному _id не найден");
+      throw new NotFound('Пользователь по указанному _id не найден');
     })
     .then((user) => res.send(user))
     .catch(next);
@@ -140,21 +141,17 @@ const updateUser = (req, res, next) => {
   const { name, email } = req.body;
   const userId = req.user._id;
 
-  User.findByIdAndUpdate(
-    userId,
-    { name, email },
-    { new: true, runValidators: true }
-  )
+  User.findByIdAndUpdate(userId, { name, email }, { new: true, runValidators: true })
     .orFail(() => {
-      throw new NotFound("Пользователь с указанным _id не найден");
+      throw new NotFound('Пользователь с указанным _id не найден');
     })
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "ValidationError" || err.name === "CastError") {
-        throw new BadRequest("Переданы некорректные данные");
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        throw new BadRequest('Переданы некорректные данные');
       }
       if (err.code === 11000) {
-        throw new ConflictError("Пользователь с таким email уже существует");
+        throw new ConflictError('Пользователь с таким email уже существует');
       }
       next(err);
     })
@@ -164,30 +161,18 @@ const updateUser = (req, res, next) => {
 const createUser = (req, res, next) => {
   const { name, email, password } = req.body;
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash) =>
-      User.create({
-        name,
-        email,
-        password: hash,
-      })
-    )
-    .then(({ _id }) => User.findById(_id))
-    .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        `${NODE_ENV === "production" ? JWT_SECRET : "yandex-praktikum"}`,
-        { expiresIn: "7d" }
-      );
-      res.send({ ...user, token: token });
-    })
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, email, password: hash,
+    }))
+    .then((({ _id }) => User.findById(_id)))
+    .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "ValidationError") {
-        throw new BadRequest("Переданы некорректные данные");
+      if (err.name === 'ValidationError') {
+        throw new BadRequest('Переданы некорректные данные');
       }
       if (err.code === 11000) {
-        throw new ConflictError("Пользователь с таким email уже существует");
+        throw new ConflictError('Пользователь с таким email уже существует');
       }
       next(err);
     })
@@ -199,11 +184,7 @@ const login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        `${NODE_ENV === "production" ? JWT_SECRET : "yandex-praktikum"}`,
-        { expiresIn: "7d" }
-      );
+      const token = jwt.sign({ _id: user._id }, `${NODE_ENV === 'production' ? JWT_SECRET : 'yandex-praktikum'}`, { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
